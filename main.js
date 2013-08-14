1a2462653af8d48938d0e4188af3c29fdc9ef6a5
Wesley Carr <surfswell17@gmail.com>
2013-08-14 16:59:54 -0400

    Full code

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Creates an object
 var Project = Backbone.Model.extend({
+  //STEP 5: This s a model of our data that describes its behavior
   idAttribute: 'slug',
   defaults: {
     name: "Default Project",
@@ -7,35 +8,29 @@ var Project = Backbone.Model.extend({
     github_url: "http://github.com/Swelly/portfolio",
     live_url: "http://Instagram.com/WesC17",
     thumbnail_url: "http://github.com/Swelly/portfolio/preview.jpg",
-    description: "Here's me awesome portfolio page"
+    description: "Here's me awesome portfolio page. Can\'t you see I like making portfolios?"
   }
 });
 
-// This is replaced by the above 'var Project'
-var project = {
-  name: "Wesley Carr",
-  github_url: "http://github.com/Swelly/portfolio",
-  live_url: "http://Instagram.com/WesC17",
-  thumbnail_url: "http://github.com/Swelly/portfolio/preview.jpg",
-  description: "Here's me awesome portfolio page"
-};
-
-// This needs to be replaced with a Collection
-var projects = {projects: [project, project, project] };
-
+// STEP 6: This is a collection that represents a set of our data
+// Like an array with superpowers
+var Projects = Backbone.Collection.extend({
+  model: Project
+});
 
 var ProjectListView = Backbone.View.extend({
-  tag_name: 'li',
-  initialize: function () {
+  // STEP 12. Setup and render the individual project
+  tagName: 'li',
+  initialize: function() {
     // Don't need to do anything here yet
   },
-  render: function () {
-    // Handlebars stuff goes here
+  render: function() {
+    // STEP 13: Handlebars stuff goes here
     var source = $('#project-template').html(),
-        template = Handlebars.compile(source),
-        data = this.model.toJSON(),
-        templatedHTML = template(data);
-    this.$el.html(templatedHTML);
+      template = Handlebars.compile(source),
+      data = this.model.toJSON(),
+      templateHTML = template(data);
+    this.$el.html(templateHTML);
 
     // Allows for function chaining
     return this;
@@ -43,61 +38,65 @@ var ProjectListView = Backbone.View.extend({
 });
 
 var AppView = Backbone.View.extend({
+  // STEP 8: This sets up the main page
   el: $('#main'),
-  initialize: function () {
-    // Populate the application with the AppView template
-
+  initialize: function() {
+    // STEP 9: Populate the application with my appView template
+    var appViewTemplate = $('#appView-template').html()
+    this.$el.html(appViewTemplate);
     // Cache commonly used selectors
+    this.list = $('#project-list');
   },
-  render: function () {
-    // Create views for each Project in the Projects collection
+  render: function() {
+    // STEP 10: Create views for each Project in the Projects collection
+    this.collection.each(function(project){
+      // STEP 11: Generate the backbone view object and set the model
+      // Each project gets its own view
+      var view = new ProjectListView({model: project});
 
+      // Using our cached object from initialize, append the new view to the list
+      this.list.append(view.render().el);
+    }, this);
+
+    // For method chaining
+    return this;
   }
 });
 
-// COLLECTION DONE
-var Projects = Backbone.Collection.extend({
-  model: Project
-});
 
-// Router
+
+// This is a bit of a Router/Controller combination
 var AppRouter = Backbone.Router.extend({
-  routes: {
-    // url: action
-    '': 'index',
-    'project/:slug': 'getProject' //Save and come back
+  // STEP 3: Setup Routes
+  routes:{
+    // url:method
+    '':'index',
+    'project/:slug':'getProject' // Save this and come back
   },
-  initialize: function (options) {
-    this.projects = new Projects([
-                new Project({name: "First Project", slug: "First-Project"}),
-                new Project({name: "Second Project", slug: "Second-Project"}),
-                new Project({name: "Three Project", slug: "Third-Project"})
-    ]);
+  initialize: function() {
+    // STEP 4: intialize routes with data.
+    // This gives me app.projects
+     this.projects = new Projects([
+        new Project({name: "First Project", slug: "first-project"}),
+        new Project({name: "Second Project", slug: "second-project"}),
+        new Project({name: "Third Project", slug: "third-project"})
+      ]);
   },
-  index: function () {
-    var appView = new ProjectListView({
-      collection: this.projects
-    });
-    projectList.render();
+  index: function() {
+    // STEP 7: When someone goes to the index. This happens
+    // Create a new instance of our AppView, pass in all our projects
+    var appView = new AppView({collection: this.projects});
+    // STEP 9: Calls render on appView
+    appView.render();
   },
-  getProject: function () {}
+  getProject: function() {}
 });
 
-
-$(function () {
-  // Handlebars.registerPartial('project', $('#project-template').html());
-
-  // // Loads the project template
-  // var source = $('#projects-template').html();
-
-  // // Renders the html into an html template
-  // var template = Handlebars.compile(source);
-
-  // // Inserts the data into the template and renders the templatedHTML
-  // var templateHTML = template(projects);
-
-  // // Appends the templatedHTML to the #main div
-  // $('#main').append(templateHTML);
-
+$(function() {
+  // STEP 1: Create instance of Router
   var app = new AppRouter();
+
+  // STEP 2: Start History
+  Backbone.history.start();
+
 });
