56fd805ef3e5b6b209359ec7301ad1206238a422
Wesley Carr <surfswell17@gmail.com>
2013-08-14 14:17:11 -0400

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+//
+var project = {
+  name: "Wesley Carr",
+  github_url: "http://github.com/Swelly/portfolio",
+  live_url: "http://Instagram.com/WesC17",
+  thumbnail_url: "http://github.com/Swelly/portfolio/preview.jpg",
+  description: "Here's me awesome portfolio page"
+};
